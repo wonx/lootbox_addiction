92ac017ec3feddac2e438747ccceb9ac45d15199
Marc Palaus <marcpalaus@gmail.com>
2023-02-16 20:55:24 +0000

    Display help if no command is passed
    Former-commit-id: 08d57e6d04d866beb7633e58b883c8b8bfceb623

--- data_ingestion/generate_df_purchases.py
+++ data_ingestion/generate_df_purchases.py
@@ -35,13 +35,15 @@ def append_new_data(csv_path="../data_ingestion/csv/", output_file="../processed
     # Append new info to main dataframe and remove duplicate rows
     main_df = pd.concat([main_df, pd.read_csv(csv_path + most_recent_csv)], ignore_index=True, axis=0)
     main_df.drop_duplicates(inplace=True)
-    print("  After concatenating:", main_df.shape)
+    print("  After concatenating: ", main_df.shape)
     main_df.to_pickle(output_file)
     print("...done")
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.set_defaults(func=lambda x: parser.print_help())
+    
     parser.add_argument('--generate-full-dataframe', action='store_true',
                         help='Generate full dataframe')
     parser.add_argument('--append-new-data', action='store_true',
@@ -50,10 +52,18 @@ if __name__ == '__main__':
                         help='Output pickle dataframe name with path')
     parser.add_argument('--output_file', type=str, default='../processed_dataframes/df_purchases.pkl',
                         help='Output pickle dataframe name with path')
+    
     args = parser.parse_args()
+  
+    
+    if not args.generate_full_dataframe and not args.append_new_data: # show help if no parameters are passed.
+        parser.print_help()
 
     if args.generate_full_dataframe:
         generate_full_dataframe(args.csv_path, args.output_file)
         
     if args.append_new_data:
         append_new_data(args.csv_path, args.output_file)
+
+        
+    
